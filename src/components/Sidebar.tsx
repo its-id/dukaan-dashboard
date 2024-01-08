415e4eeab8c4cfb1f5723af4dc8a6f0075d131f6
its-id <ik.dana24@gmail.com>
2024-01-09 02:01:26 +0530

    complete sidebar bottom

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { SidebarProps } from '../utils/types';
 
 //icon imports
 import crossIcon from '../assets/icons/cross.svg';
+import walletIcon from '../assets/icons/wallet.svg';
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ');
@@ -44,7 +45,7 @@ const Sidebar = ({
             leaveFrom='translate-x-0'
             leaveTo='-translate-x-full'
           >
-            <div className='relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-800'>
+            <div className='relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-[#1E2640]'>
               <Transition.Child
                 as={Fragment}
                 enter='ease-in-out duration-300'
@@ -136,7 +137,7 @@ const Sidebar = ({
       {/* Static sidebar for desktop */}
       <div className='hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0'>
         {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className='flex-1 flex flex-col min-h-0 bg-gray-800'>
+        <div className='flex-1 flex flex-col min-h-0 bg-[#1E2640] py-4 px-2.5'>
           <div className='flex items-center h-16 flex-shrink-0 px-4 bg-gray-900'>
             <img
               className='h-8 w-auto'
@@ -172,21 +173,21 @@ const Sidebar = ({
               ))}
             </nav>
           </div>
-          <div className='flex-shrink-0 flex bg-gray-700 p-4'>
+          <div className='flex-shrink-0 flex bg-[#353C53]/90 py-1.5 px-3 rounded'>
             <a href='#' className='flex-shrink-0 w-full group block'>
-              <div className='flex items-center'>
-                <div>
+              <div className='flex items-center gap-3'>
+                <div className='rounded bg-white/10 p-1.5'>
                   <img
-                    className='inline-block h-9 w-9 rounded-full'
-                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                    className='inline-block h-6 w-6'
+                    src={walletIcon}
                     alt=''
                   />
                 </div>
-                <div className='ml-3'>
-                  <p className='text-sm font-medium text-white'>Tom Cook</p>
-                  <p className='text-xs font-medium text-gray-300 group-hover:text-gray-200'>
-                    View profile
+                <div className='flex flex-col gap-0.5 text-white'>
+                  <p className='text-[13px] font-medium'>
+                    Available credits
                   </p>
+                  <p className='font-medium group-hover:text-gray-50'>222.10</p>
                 </div>
               </div>
             </a>
